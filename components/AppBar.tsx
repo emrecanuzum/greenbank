63070d79a500c5ae2abd834df19db397c6ed1805
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-08-13 05:50:37 +0300

    Versioning untracked files

--- components/AppBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MyMultiButton from './MyMultiButton';
-
-export default function PrimarySearchAppBar() {
-  return (
-              <MyMultiButton />
-  );
-}
--- pages/api/analytics.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-         performance: '3s',
-         clients: '643',
-         transactions: '+142K',
-         funds: '+123M'
-        });    
-  }
